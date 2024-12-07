7d5af2041ad2a49f96fee450bcc5386ce0ae13d3
Amine Aouragh <amine.aouragh.20@gmail.com>
2024-12-07 18:18:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let loginbtn = document.querySelector(".login-btn")
+let masterbundle = document.getElementById("master-bundle")
+
+loginbtn.addEventListener("click", () => {
+    masterbundle.style.display = "none";
+})
